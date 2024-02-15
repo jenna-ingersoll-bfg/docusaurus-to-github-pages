62bfc16b26e2594fa3849f28718684c292742b01
jenna-ingersoll-bfg <62573960+jenna-ingersoll-bfg@users.noreply.github.com>
2024-02-14 16:16:52 -0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'The ANCHOR',
+  tagline: 'Your gateway into Big Fish's documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
