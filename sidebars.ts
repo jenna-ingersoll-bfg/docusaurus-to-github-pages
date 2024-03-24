2078cf3903f39ee791a661555e82079d2732217f
jenna-ingersoll-bfg <62573960+jenna-ingersoll-bfg@users.noreply.github.com>
2024-03-23 21:51:15 -0700

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -1,6 +1,6 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 export default {
-  devSidebar: ['static/bfg_welcome', 'static/dev_onboarding', 'static/dev_security', 'static/prod_welcome'],
+  devSidebar: ['static/dev_welcome', 'static/dev_onboarding', 'static/dev_security', 'static/prod_welcome'],
   sdkSidebar: ['bfgsdk/bfgsdk_welcome', 'bfgsdk/sdktest'],
 };
