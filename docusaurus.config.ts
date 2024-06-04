bbe6a371fe65d63a1f2dbbab80aa2bb77c1a04dd
Jenna <jenna.ingersoll@bigfishgames.com>
2024-06-04 13:01:38 -0700

    docker

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -150,7 +150,7 @@ const config: Config = {
       typesenseServerConfig: {
         nodes: [
           {
-            host: 'localhost',
+            host: 'host.docker.internal',
             port: 80,
             protocol: 'http',
           },
