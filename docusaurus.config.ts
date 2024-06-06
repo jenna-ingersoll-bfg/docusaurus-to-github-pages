b454bde985104d7640713d85bb699af94071407a
Jenna <jenna.ingersoll@bigfishgames.com>
2024-06-06 14:19:29 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
         {
           type: 'doc',
           position: 'right',
-          docPluginId: 'internal',
+          docID: '/internal/producer/prod_welcome',
           label: 'BFG Internal',
         },
       ],
